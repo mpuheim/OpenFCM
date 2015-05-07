ea5c70fdfccf15182e2e691c21cf099cbd9947a9
Michal Puheim <michal.puheim@tuke.sk>
2015-05-07 17:18:20 +0200

    Added simulator project
    - added simulator (GUI)
    - reworked solution folder structure

---
+++ library/FCM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace library
+{
+    public class FCM
+    {
+        #region INFO
+        /*  
+            load in Matlab using:
+            NET.addAssembly('E:\Data\Programming\FuzzyCognitiveMapsLibrary\libFCM\libFCM\bin\Debug\libFCM.dll')
+        */
+        #endregion
+
+
+    }
+}
